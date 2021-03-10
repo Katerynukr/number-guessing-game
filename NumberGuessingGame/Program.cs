11bded94d898c3c97205e5060ae49d82fa69d384
Katerynukr <kateryna_neholiuk@ukr.net>
2021-03-10 12:38:58 +0200

    exceptions data convertation loop

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -6,7 +6,65 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int number = random.Next(100);
+            Console.WriteLine(number);
+            int userGuess = 0;
+            int numberOfGuesses = 5;
+            bool guessed = false;
+            Console.WriteLine("I guessed a number from 1 to 100. You can try to guess it.");
+            Console.WriteLine("Write your assumption:");
+            //try
+            //{
+                String input = Console.ReadLine().Trim();
+                while(!guessed && numberOfGuesses > 0)
+                {
+                    numberOfGuesses--;
+                    if (input.Length > 3)
+                    {
+                        throw new Exception("The number is too long");
+                    }
+                try
+                {
+                    userGuess = Convert.ToInt32(input);
+
+                    if (userGuess > number)
+                    {
+                        Console.WriteLine($"The number {userGuess} is larger than my guess number.");
+                    }
+                    else if (userGuess < number)
+                    {
+                        Console.WriteLine($"The number {userGuess} is smaller than my guess number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Well done. You guessed it!");
+                        guessed = true;
+                        break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You entered invalid data type. Please enter numbers only!");
+
+                }
+                if(numberOfGuesses < 0)
+                {
+                    Console.WriteLine("Sorry to say it. But you lost!");
+                }
+                Console.WriteLine($"You have {numberOfGuesses} number of guesses left. Try again, I belive in you!");
+                Console.WriteLine("Make a guess one more time:");
+                input = Console.ReadLine().Trim();
+                    
+                }
+
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("You entered invalid data type. Please enter numbers only!");
+            //}
+            
+
         }
     }
 }
